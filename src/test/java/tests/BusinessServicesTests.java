e7a8c9aaca4cb2e60392d3d46f2500e625eda015
Igor Pavlov <igortvk@ya.ru>
2020-12-14 21:16:51 +0300

    refactoring

---
+++ src/test/java/tests/BusinessServicesTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class BusinessServicesTests extends TestBase {
+
+    @Test
+    @DisplayName("Проверить раздел Тарифы МСБ")
+    void  successfulOpenTariffPage() {
+        step("Открыть главную страницу", () ->
+                open("https://sovcombank.ru/business"));
+        $(".CookieOffer__buttons").click();
+
+        step("Перейти на страницу 'Подобрать тариф  для бизнеса' ", () ->
+                $x("//*[@id='__next']/div/main/section[1]/div/a[2]").click());
+        switchTo().window(1);
+
+        step("Проверить наличие на страницы вкладов", () ->
+                $("html").findAll("span.Products__item__inner").shouldHaveSize(4));
+
+        step("Перейти во вклад Успех",()->
+                $(byText("Успех")).click());
+
+        step("Проверить успешность открытия страницы Вклада", () ->
+                $("h2.Typography").shouldHave(text("Условия")));
+    }
+    @Test
+    @DisplayName("Проверить раздел Тарифа МСБ - Негативный сценарий")
+    void unsuccessfulOpenTariffPage() {
+        step("Открыть главную страницу", () ->
+                open("https://sovcombank.ru/business"));
+        $(".CookieOffer__buttons").click();
+
+        step("Перейти на страницу 'Подобрать тариф  для бизнеса' ", () ->
+                $x("//*[@id='__next']/div/main/section[1]/div/a[2]").click());
+        switchTo().window(1);
+
+        step("Проверить наличие на страницы вкладов", () ->
+                $("html").findAll("span.Products__item__inner").shouldHaveSize(4));
+
+        step("Перейти во вклад Доходный",()->
+                $(byText("Доходный")).click());
+
+        step("Проверить успешность открытия страницы Вклада", () ->
+                $("h1.Typography").shouldHave(text("Успех"))); //ДОХОДНЫЙ
+
+    }
+}
+
+
+
+
+
+
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,50 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+import static helpers.AttachmentsHelper.attachVideo;
+
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        String selenoidLogin = System.getProperty("selenoid.username");
+        String selenoidPwd = System.getProperty("selenoid.pwd");
+        String remoteBrowser = System.getProperty("remote.browser.url");
+        String url = "https://"+ selenoidLogin + ":" + selenoidPwd  + "@" + remoteBrowser + ":4444/wd/hub/";
+
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.remote = url;
+        Configuration.browserCapabilities = capabilities;
+        Configuration.startMaximized = true;
+        //Configuration.browser = FIREFOX;
+    }
+
+    @AfterEach
+    @Step("Attachments")
+
+
+    public void afterEach () {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        attachVideo();
+
+        closeWebDriver();
+    }
+}
+
+
